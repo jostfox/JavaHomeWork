8fbef7ed0c0067ce1aade946a854ebc79994a937
Oleg Rulyov <johstfox@gmail.com>
2022-11-27 22:26:12 +0100

    Initial commit

---
+++ src/HomeWorkNineLTwo.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class HomeWorkNineLTwo {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Сумма вклада: ");
+        int amount = scanner.nextInt();
+        System.out.println("Годовой %: ");
+        double yearlyPercent = scanner.nextDouble();
+        System.out.println("Срок: ");
+        int termin = scanner.nextInt();
+
+        for (int i = 0; i < termin ; i++) {
+            amount = amount + (int)(amount*(yearlyPercent/100));
+        }
+        System.out.printf("Через %d лет вклад будет %d EUR", termin, amount);
+
+
+
+
+
+
+    }
+}
