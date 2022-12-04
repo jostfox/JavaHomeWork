705f572b721ce9a0707684bfaf2b732db148c7e6
Oleg Rulyov <johstfox@gmail.com>
2022-12-04 18:51:44 +0100

    Initial commit. Sum of the array added

--- src/HomeWorkTenLTwo.java
+++ src/HomeWorkTenLTwo.java
@@ -5,11 +5,14 @@ public class HomeWorkTenLTwo {
     public static void main(String[] args) {
 
         int[] myArray = new int[100];
+        int sum = 0;
         for (int i = 0; i < myArray.length; i++) {
             myArray[i] = (i % 10) * 111;
+            sum = sum + myArray[i];
         }
 
         System.out.println(Arrays.toString(myArray));
+        System.out.printf("Sum of array is %d", sum);
 
     }
 
